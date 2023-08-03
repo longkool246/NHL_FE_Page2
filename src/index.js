acc6571d08f87452ab1deb29ecd7a1c87d94d637
Nguyen Hai Long <longnh@timebird.org>
2023-08-03 21:17:55 +0700

    first-commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+window.addEventListener("scroll", function() {
+  var header = document.querySelector(".header");
+  if (window.scrollY > 0) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+});
